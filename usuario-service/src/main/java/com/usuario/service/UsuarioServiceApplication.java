562681b92a578515ca9eb98f89ebedfa97f74ae7
Alberto Sanchez <albertosanchez15111rgmail.com>
2024-09-22 01:03:27 +0200

    eureka

--- usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
 @EnableFeignClients
 @SpringBootApplication
+
 public class UsuarioServiceApplication {
 
 	public static void main(String[] args) {
